7d33107b187a1c4c43998ab981cb325c05b7125f
Jakub Mihuka <jakubmihulka02@gmail.com>
2022-09-26 17:56:14 +0200

    fix

---
+++ src/main/java/com/crud/tasks/domain/dto/TaskDto.java
@@ -0,0 +1,13 @@
+package com.crud.tasks.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TaskDto {
+    private Long id;
+    private String title;
+    private String content;
+
+}
