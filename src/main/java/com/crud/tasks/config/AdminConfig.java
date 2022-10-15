f3b270ec51eb3cef178d5416b64856be6f3cd5e1
Jakub Mihuka <jakubmihulka02@gmail.com>
2022-10-15 13:06:04 +0200

    Zadanie 32.2

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -10,4 +10,8 @@ public class AdminConfig {
 
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
 }
---
+++ src/main/java/com/crud/tasks/config/CompanyConfig.java
@@ -0,0 +1,21 @@
+package com.crud.tasks.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class CompanyConfig {
+    @Value("${info.app.name}")
+    private String companyName;
+
+    @Value("${info.app.owner.name}")
+    private String companyOwnerFirstName;
+
+    @Value("${info.app.owner.surname}")
+    private String companyOwnerLastName;
+
+    @Value("${info.app.administrator.email}")
+    private String companyEmail;
+}
